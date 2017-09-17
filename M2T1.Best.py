9a754c34dd70357d4de4e728fb3011445f97fb65
bests4413 <32025211+bests4413@users.noreply.github.com>
2017-09-16 20:42:16 -0400

    Add files via upload
    First submission.

---
+++ M2T1.Best.py
@@ -0,0 +1,18 @@
+#Module two example one
+# Calculating a result
+# Best
+# 9-16-2017
+
+totalSales = 10000.00
+commissionPct = 0.08
+commission = totalSales * commissionPct
+
+print("Your Commission Is: ", commission)
+
+hours = int(input('Input hours worked: '))
+payRate = float(input("Input Hourly Pay: "))
+grossPay = hours * payRate
+
+print("Gross Pay Is: ", grossPay)
+input("Press ENTER To Exit Program. ")
+
